46d5ab96770eaf0991b9bae0d86d296c6a9a2e80
ankitbhusal <ankitbhusal95@gmail.com>
2021-07-06 21:24:54 +0545

    0.1

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToScroll: 1,
           infinite: true,
           dots: true
         }
@@ -20,7 +20,7 @@
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2
+          slidesToScroll: 1
         }
       },
       {
