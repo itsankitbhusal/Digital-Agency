9360a569e0273f8018b4dc53995cd3362a3455ca
ankitbhusal <ankitbhusal95@gmail.com>
2021-07-07 08:19:17 +0545

    0.1

--- js/script.js
+++ js/script.js
@@ -14,11 +14,13 @@ $(".responsive").slick({
         slidesToShow: 3,
         slidesToScroll: 1,
         infinite: true,
-        dots: true,
+        dots: false,
+        mobileFirst:true,
+        variableWidth: true,
       },
     },
     {
-      breakpoint: 600,
+      breakpoint: 1024,
       settings: {
         slidesToShow: 2,
         slidesToScroll: 1,
